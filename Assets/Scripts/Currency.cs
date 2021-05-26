2d5dfde0d925d61241ab7c429e5f0ef698641f6d
guimendel <guimendeln@gmail.com>
2021-05-26 12:25:09 +0200

    add currency text

---
+++ Assets/Scripts/Currency.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Currency : MonoBehaviour
+{
+  [SerializeField] int count = 3;
+  TextMeshProUGUI textComponent;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    textComponent = GetComponent<TextMeshProUGUI>();
+    UpdateText();
+  }
+
+  private void UpdateText()
+  {
+    textComponent.text = count.ToString();
+  }
+
+  public int UpdateBy(int amount)
+  {
+    if (count + amount < 0) throw new InsufficientFundsException("Not enough currency!");
+
+    count += amount;
+    UpdateText();
+    return count;
+  }
+}
